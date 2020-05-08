4d45ba4e29cc3f4deeb0f4813e88b10d7f9e9f8e
Jacob Stanger <jstangwork@gmail.com>
2020-05-08 12:21:16 -0600

    toggle between 2 pages.

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,21 +3,21 @@ import React from 'react';
 export default function Navbar() {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a class="navbar-brand" href="#">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand" href="/">
                     <img src="./logo.svg" width="30" height="30" className="d-inline-block align-top" alt="" />
                     ReactReactGo
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Search <span class="sr-only">(current)</span></a>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/">Search <span className="sr-only">(current)</span></a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Saved </a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/saved-books">Saved </a>
                         </li>
                     </ul>
                 </div>
