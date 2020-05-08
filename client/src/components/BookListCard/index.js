7371ac3f7bffcbb7c911c2e66cf588fe30eb3eeb
Jacob Stanger <jstangwork@gmail.com>
2020-05-08 14:20:08 -0600

    default stock image for books that don't come back with imageurl

--- client/src/components/BookListCard/index.js
+++ client/src/components/BookListCard/index.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import './index.css';
 
-export default function BookListCard() {
+export default function BookListCard(props) {
     /* TODO: optional - cards have a fight when they are on mobile  */
     
     return (
         <div className="container-fluid card">
             <div className="row">
                 <div className="card-body col-10">
-                    <h2>Book name</h2>
+                    <h2>{props.bookTitle}</h2>
                 </div>
                 <div className="col-2 card-body align-self-end">
                     <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-                        <button type="button" className="btn btn-warning">View</button>
+                        <a href={props.bookUrl} className="btn btn-warning">View</a>
                         <button type="button" className="btn btn-info">Save</button>
                     </div>
                 </div>
             </div>
             <div className="row">
-                <div className="card-body col-2">
-                    <img src="https://via.placeholder.com/150" alt="book cover" />
+                <div className="card-body col-3">
+                    <img src={props.imageUrl} alt="book cover" />
                 </div>
-                <div className="col-10 card-body align-self-end">
+                <div className="col-9 card-body align-self-end">
                     <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam feugiat leo libero, vitae consectetur eros vulputate id. Proin facilisis aliquet dui, in interdum lorem imperdiet sodales. Morbi lacinia fermentum lacus sed gravida. Morbi fringilla porttitor tortor, eget aliquam lectus viverra nec. Curabitur vel justo justo. Fusce rhoncus convallis libero non mattis. Cras iaculis nisi nulla, id ultricies sem vestibulum at. Curabitur nec lacus magna. Integer dignissim enim ut mi fringilla suscipit. Nunc vitae arcu sed tortor posuere accumsan et vel libero. Integer mattis odio vestibulum risus vulputate, vulputate consectetur mauris posuere.
                     </p>
--- client/src/components/pages/savedBooks.js
+++ client/src/components/pages/savedBooks.js
@@ -1,21 +1,61 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+import BookListCard from '../BookListCard';
 import ResultBookList from '../ResultsBookList';
 
-export default function SavedBooks() {
+class SavedBooks extends Component {
+    constructor(props) {
+        super(props);
 
-    const [ bookTitle, setBookTitle ] = useState('');
-    const [ savedBookList, setBookList ] = useState([]);
+        this.state = {
+            defaultImageUrl: "https://source.unsplash.com/sfL_QOnmy00/200x200",
+            listOfSavedBooks: [
+                {
+                    title: 'The Hunger Games',
+                    authors: ['Suzanne Collins'],
+                    description: "Set in a dark vision of the near future, a terrifying reality TV show is taking place. Twelve boys and twelve girls are forced to appear in a live event called The Hunger Games. There is only one rule: kill or be killed. When sixteen-year-old Katniss Everdeen steps forward to take her younger sister's place in the games, she sees it as a death sentence. But Katniss has been close to death before. For her, survival is second nature.",
+                    imageUrl: "http://books.google.com/books/content?id=sazytgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+                    bookUrl: "http://books.google.com/books?id=sazytgAACAAJ&dq=title:The+Hunger+Games&hl=&source=gbs_api",
+                },
+                {
+                    title: 'I am Legend',
+                    authors: ['Richard Matheson'],
+                    description: "",
+                    imageUrl: "",
+                    bookurl: ""
+                },
+                {
+                    title: 'The Hobbit',
+                    authors: ['J.R.R Tolkein'],
+                    description: "",
+                    imageUrl: "",
+                    bookurl: ""
+                }
+            ]
+        }
+    }
 
-    
 
-    return (
-        <div className="container">
-            <div className="jumbotron">
-                <h1 className="display-4">ReactReactGo</h1>
-                <p className="lead">Search for and save books of interest</p>
-                <hr className="my-4" />
+    render(props) {
+        return (
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">ReactReactGo</h1>
+                    <p className="lead">Search for and save books of interest</p>
+                    <hr className="my-4" />
+                </div>
+                {
+                    this.state.listOfSavedBooks.map((book, index) => (
+                        <BookListCard 
+                            key={index}
+                            bookTitle={book.title}
+                            imageUrl={book.imageUrl || this.state.defaultImageUrl}
+                            bookUrl={book.bookUrl}
+                        />
+                    ))
+                }
             </div>
-            <ResultBookList />
-        </div>
-    );
+        );
+    }
 }
+
+export default SavedBooks;
