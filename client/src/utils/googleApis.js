faef4ecd5bb03c5d0c3d0fd4ec53955fe4c9a659
Jacob Stanger <jstangwork@gmail.com>
2020-05-11 21:23:54 -0600

    updated google api to 20s

--- client/src/utils/googleApis.js
+++ client/src/utils/googleApis.js
@@ -2,8 +2,12 @@ import axios from 'axios';
 
 export default function googleBooksApiRoutes (searchTerm) {
     const apiKey = 'AIzaSyDmrv-BNf605fTWjdYmc2ReKWSqkNuNHeY';
+    console.log(searchTerm);
     const formattedSearchTerm = searchTerm.split(' ').join('+');
-    const endpoint = `https://www.googleapis.com/books/v1/volumes?q=${formattedSearchTerm}&key=${apiKey}`;
+    console.log(formattedSearchTerm);
+    const formattedSearchTerm2 = encodeURIComponent(searchTerm.trim());
+    console.log(formattedSearchTerm2);
+    const endpoint = `https://www.googleapis.com/books/v1/volumes?q=${formattedSearchTerm2}&key=${apiKey}`;
 
     return axios.get(endpoint);
 
