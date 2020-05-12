bf167e81b3d83b95a24089c737022e2a42850233
Jacob Stanger <jstangwork@gmail.com>
2020-05-11 21:46:12 -0600

    cleanup

--- client/src/utils/googleApis.js
+++ client/src/utils/googleApis.js
@@ -2,11 +2,8 @@ import axios from 'axios';
 
 export default function googleBooksApiRoutes (searchTerm) {
     const apiKey = 'AIzaSyDmrv-BNf605fTWjdYmc2ReKWSqkNuNHeY';
-    console.log(searchTerm);
     const formattedSearchTerm = searchTerm.split(' ').join('+');
-    console.log(formattedSearchTerm);
     const formattedSearchTerm2 = encodeURIComponent(searchTerm.trim());
-    console.log(formattedSearchTerm2);
     const endpoint = `https://www.googleapis.com/books/v1/volumes?q=${formattedSearchTerm2}&key=${apiKey}`;
 
     return axios.get(endpoint);
