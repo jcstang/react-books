bd26bbe0af92ccc52ab66d2a3f9efd1a7c3cff19
Jacob Stanger <jstangwork@gmail.com>
2020-05-30 15:40:10 -0600

    sample data

---
+++ client/src/components/pages/data/sample 2.js
@@ -0,0 +1,23 @@
+const sampleMongoData = [
+    {
+        title: 'The Hunger Games',
+        authors: ['Suzanne Collins'],
+        description: "Set in a dark vision of the near future, a terrifying reality TV show is taking place. Twelve boys and twelve girls are forced to appear in a live event called The Hunger Games. There is only one rule: kill or be killed. When sixteen-year-old Katniss Everdeen steps forward to take her younger sister's place in the games, she sees it as a death sentence. But Katniss has been close to death before. For her, survival is second nature.",
+        imageUrl: "http://books.google.com/books/content?id=sazytgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+        bookUrl: "http://books.google.com/books?id=sazytgAACAAJ&dq=title:The+Hunger+Games&hl=&source=gbs_api",
+    },
+    {
+        title: 'I am Legend',
+        authors: ['Richard Matheson'],
+        description: "",
+        imageUrl: "",
+        bookUrl: ""
+    },
+    {
+        title: 'The Hobbit',
+        authors: ['J.R.R Tolkein'],
+        description: "",
+        imageUrl: "",
+        bookUrl: ""
+    }
+];
