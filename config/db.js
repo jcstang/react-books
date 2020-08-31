9e3f1808eb018891bac20ba06a58900a4fce0ec5
Jacob Stanger <jacobcstanger@gmail.com>
2020-08-31 14:17:19 -0600

    console log on process

--- config/db.js
+++ config/db.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose');
 
 
 const connectDB = async () => {
+  
+  console.log(`... process.env.NODE_ENV....... ${process.env.NODE_ENV}`)
 
   let MONGODB_URI = process.env.NODE_ENV
     ? process.env.MONGODB_URI
     : "mongodb://localhost/google_books";
+
 
   try {
     await mongoose.connect(MONGODB_URI, {
